0ca7c533faf51d4836027935c3e76a744c0c5004
johnny1204 <major@za2.so-net.ne.jp>
2017-10-10 18:26:54 +0900

    tutorial

---
+++ sample_app/test/controllers/users_controller_test.rb
@@ -0,0 +1,56 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+    @non_activated_user = users(:non_activated)
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params:{
+      user: {
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { 
+        password: "foobar",              password_confirmation: "foober",admin: true 
+      } 
+    }
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should not allow the not activated attribute" do
+    log_in_as(@non_activated_user)
+    assert_not @non_activated_user.activated?
+    get users_path
+    assert_select "a[href=?]", user_path(@non_activated_user), count:0
+    get user_path(@non_activated_user)
+    assert_redirected_to root_url
+  end
+end
